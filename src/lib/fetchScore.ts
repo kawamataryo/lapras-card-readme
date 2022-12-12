7cf7a0f55b1378f731df780a5e40f774dc3adca6
kawamataryo <ba068082@gmail.com>
2022-12-12 20:27:29 +0900

    fix: add fetch

--- src/lib/fetchScore.ts
+++ src/lib/fetchScore.ts
@@ -1,4 +1,6 @@
 import {PublicApiResponse, Score} from '../types/types'
+import fetch from 'node-fetch'
+
 
 export const fetchScore = async (shareId: string): Promise<Score> => {
   const res = await fetch(`https://lapras.com/public/${shareId}.json`)
