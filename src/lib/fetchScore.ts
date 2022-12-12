c5fb96a4af991d01afe46906733298fdc934f87b
kawamataryo <ba068082@gmail.com>
2022-12-12 19:54:39 +0900

    fix: error handling

--- src/lib/fetchScore.ts
+++ src/lib/fetchScore.ts
@@ -1,21 +1,12 @@
 import {PublicApiResponse, Score} from '../types/types'
 
 export const fetchScore = async (shareId: string): Promise<Score> => {
-  try {
-    const res = await fetch(`https://lapras.com/public/${shareId}.json`)
-    const response = (await res.json()) as PublicApiResponse
+  const res = await fetch(`https://lapras.com/public/${shareId}.json`)
+  const response = (await res.json()) as PublicApiResponse
 
-    return {
-      eScore: response.e_score,
-      bScore: response.b_score,
-      iScore: response.i_score
-    }
-  } catch {
-    // TODO: remove this fallback
-    return {
-      eScore: 4.26,
-      bScore: 3.48,
-      iScore: 4.05
-    }
+  return {
+    eScore: response.e_score,
+    bScore: response.b_score,
+    iScore: response.i_score
   }
 }
